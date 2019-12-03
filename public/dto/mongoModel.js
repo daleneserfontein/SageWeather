ef6211e5bca8f3b2c4694910fa45904a9d84a886
Dalene Serfontein <dalene.serfontein@sage.com>
2019-12-03 12:20:44 +0200

    add butterfly images

--- public/dto/mongoModel.js
+++ public/dto/mongoModel.js
@@ -3,7 +3,7 @@ module.exports = {
     title: 'Mongo App',
     dateToday: new Date(),
     name: '',
-    description: 'This is a mongo app',
+    description: 'This is a mongo app. Read all the search history from the weather app',
     searchResult: {
         placeName: '',
         temperature: '',
